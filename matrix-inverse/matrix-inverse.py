91d035e96abcfbea4246be141c270a33965a4e8c
Manikanta2810 <manikantac3006@gmail.com>
2026-06-22 10:13:08 +0530

    Solve matrix-inverse

--- matrix-inverse/matrix-inverse.py
+++ matrix-inverse/matrix-inverse.py
@@ -4,10 +4,8 @@ def matrix_inverse(A):
     """
     Returns: A_inv of shape (n, n) such that A @ A_inv ≈ I
     """
-    # Write code here
-    
     A = np.array(A)
-    m,n = A.shape
+    m,n =A.shape
     if m!=n:
         return None
     det_A = np.linalg.det(A)
@@ -18,8 +16,8 @@ def matrix_inverse(A):
         for j in range(n):
             minor = np.delete(A,i,axis=0)
             minor = np.delete(minor,j,axis=1)
-            minor_det = np.linalg.det(minor)
-            cof_mat[i][j] = ((-1) ** (i + j)) * minor_det
+            cof_mat[i][j]= ((-1)**(i+j))*np.linalg.det(minor)
     adj = cof_mat.T
     inverse = adj/det_A
     return inverse
+
